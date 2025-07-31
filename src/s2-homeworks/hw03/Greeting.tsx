cd1dac1312d21a2ea60e621bdcc559b9f615a4e5
Anton <a.barai6003@gmail.com>
2025-07-31 11:04:49 +0300

    sprint2-HW3-finished

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -1,15 +1,19 @@
 import React, { ChangeEvent, KeyboardEvent } from 'react'
 import s from './Greeting.module.css'
 
+// * 9 - в файле Greeting.tsx дописать типизацию пропсов
+// * 10 - в файле Greeting.tsx вычислить inputClass в зависимости от наличия ошибки
+// * 11 - сделать стили в соответствии с дизайном
+
 type GreetingPropsType = {
-  name: any // need to fix any
-  setNameCallback: any // need to fix any
-  addUser: any // need to fix any
-  onBlur: any // need to fix any
-  onEnter: any // need to fix any
-  error: any // need to fix any
-  totalUsers: any // need to fix any
-  lastUserName?: any // need to fix any
+  name: string // need to fix any
+  setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+  addUser: () => void // need to fix any
+  onBlur: () => void // need to fix any
+  onEnter: (e: KeyboardEvent<HTMLInputElement>) => void // need to fix any
+  error: string // need to fix any
+  totalUsers: number // need to fix any
+  lastUserName?: string // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
@@ -25,7 +29,7 @@ const Greeting: React.FC<GreetingPropsType> = (
     lastUserName,
   }, // деструктуризация пропсов
 ) => {
-  const inputClass = s.errorInput // need to fix with (?:)
+  const inputClass = error ? s.errorInput : s.input // need to fix with (?:)
 
   return (
     <div id={'hw3-form'} className={s.greetingForm}>
@@ -55,7 +59,7 @@ const Greeting: React.FC<GreetingPropsType> = (
           className={s.button}
           disabled={!name.trim()}
         >
-          add
+          Add
         </button>
       </div>
 
