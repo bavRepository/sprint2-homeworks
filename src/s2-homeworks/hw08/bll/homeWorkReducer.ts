db8d7eb5d5b0c098507a151c7a16d5b746c261b7
Anton <a.barai6003@gmail.com>
2025-09-08 08:57:21 +0300

    sprint2-HW8-finished-fix

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,16 +4,38 @@ type ActionType =
   | { type: 'sort'; payload: 'up' | 'down' }
   | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => {
+export const homeWorkReducer = (state: any, action: ActionType): any => {
   // need to fix any
   switch (action.type) {
     case 'sort': {
       // by name
+      const sortedNamesState = [...state]
 
-      return state // need to fix
+      if (action.payload === 'up') {
+        sortedNamesState.sort(function (a, b) {
+          if (a.name > b.name) {
+            return 1
+          }
+          if (a.name < b.name) {
+            return -1
+          }
+          return 0
+        })
+      } else {
+        sortedNamesState.sort(function (a, b) {
+          if (a.name < b.name) {
+            return 1
+          }
+          if (a.name > b.name) {
+            return -1
+          }
+          return 0
+        })
+      }
+      return sortedNamesState
     }
     case 'check': {
-      return state // need to fix
+      return state.filter((person: UserType) => person.age >= 18)
     }
     default:
       return state
