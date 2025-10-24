525ac114f6473f5c1dd9b40a72708642d4714920
Anton <a.barai6003@gmail.com>
2025-10-24 09:35:44 +0300

    sprint2-HW15-finished-fixed-v5

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -13,10 +13,11 @@ export type SuperSortPropsType = {
 
 export const pureChange = (sort: string, down: string, up: string) => {
   // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
+
   if (sort === '') return down
   if (sort === down) return up
-  if (sort === up) return down
-  return down
+  if (sort === up) return ''
+  return up
 }
 
 const SuperSort: React.FC<SuperSortPropsType> = ({
